7599592370897ddaae40ba6a132de57a37ed8fe6
Pablo Eusebio <nunieboy88@gmail.com>
2018-10-05 14:48:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,32 +113,5 @@ end
 
 
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
-# Define your play method below
-def play(board)
-  i = 1
-  player = 1
-  while i <= 9
-  turn(board)
-  i +=1
-  if player % 2 == 0
-    current_player = "O"
-    player += 1
-  end
-end
-end
